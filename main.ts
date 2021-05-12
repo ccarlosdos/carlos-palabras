63d5fced91f29eb9ec9aa4abd46b1a6698697593
ccarlosdos <84058309+ccarlosdos@users.noreply.github.com>
2021-05-11 22:46:47 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,75 @@
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
